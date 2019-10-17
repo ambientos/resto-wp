3dc4b7443d328b29fbd2c16c8e619274c01f3022
ambientos <ant.grakhov@gmail.com>
2019-10-18 02:47:01 +0500

    Add Navigation search

--- themes/resto-service/js/site.js
+++ themes/resto-service/js/site.js
@@ -124,4 +124,15 @@
 			control.trigger('change')
 		})
 	}
+
+
+	/**
+	 * Navigation Search
+	 */
+	$('.header-nav-search').on('click', function(){
+		var searchButton = $(this),
+			parent = searchButton.parent()
+
+		parent.addClass('show-control')
+	})
 })(jQuery)
