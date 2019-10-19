7b9ad837a82e89dfd77db2ff98b3d3ea5653b6ab
ambientos <ant.grakhov@gmail.com>
2019-10-20 03:31:29 +0500

    Some fix

--- themes/resto-service/archive.php
+++ themes/resto-service/archive.php
@@ -5,6 +5,7 @@
 
 	<?php if ( have_posts() ) : ?>
 		<div class="widget">
+			<h1 class="widget-title"><?php the_archive_title(); ?></h1>
 			<div class="knowledge-article-list row">
 				<?php while ( have_posts() ) : the_post(); ?>
 					<?php get_template_part( 'template-parts/content', 'article' ); ?>
